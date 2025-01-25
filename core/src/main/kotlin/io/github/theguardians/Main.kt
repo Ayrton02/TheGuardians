3d0488c0677b14987a4763439e37e61226ec0e1f
Ayrton <ayrton.0210@gmail.com>
2025-01-25 16:06:50 -0300

    first commit

---
+++ core/src/main/kotlin/io/github/theguardians/Main.kt
@@ -0,0 +1,10 @@
+package io.github.theguardians
+
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+
+class Main : KtxGame<KtxScreen>() {
+    override fun create() {
+        super.create()
+    }
+}
---
+++ core/src/main/kotlin/io/github/theguardians/screens/GameScreen.kt
@@ -0,0 +1,7 @@
+package io.github.theguardians.screens
+
+import ktx.app.KtxScreen
+
+class GameScreen: KtxScreen {
+
+}
